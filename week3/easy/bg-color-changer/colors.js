f5e9d2378c35cca722dc47489d8818e203934a38
Danish-Belal <danishexplore019@gmail.com>
2024-11-02 15:33:26 +0530

    Complete Assignment

---
+++ week3/easy/bg-color-changer/colors.js
@@ -0,0 +1,4 @@
+export const pannelColor = [
+     {"id": 1, "colour" : "red"},
+     {"id": 2, "colour" : "green"},
+ ];
--- week3/easy/bg-color-changer/script.js
+++ week3/easy/bg-color-changer/script.js
@@ -1,12 +1,8 @@
 
-const pannelColor = [
-     {"id": 1,
-     "colour" : "red"
-     },
-     {"id": 2,
-     "colour" : "green"
-     },
-]
+import {pannelColor}  from "./colors.js";
+// console.log("Pannel",pannelColor);
+
+
 const pannel = document.getElementById('pannel');
 
 
@@ -15,31 +11,83 @@ function setColor(colour){
 }
 
 function createColor(colour){
-     id = pannelColor.length+1;
-     pannelColor.push({id, colour})
-     
+     console.log("Color in createColor", colour);
+
+    // Set the new id based on the current length of pannelColor
+    const id = pannelColor.length + 1;
+    pannelColor.push({ id, colour });
+    
+    // Store the updated pannelColor array in localStorage
+    localStorage.setItem('pannelColor', JSON.stringify(pannelColor));
+    
+    console.log('Pannel Color after adding', pannelColor);
+    displayingPannel();
+    const box = document.getElementById('box');
+    console.log('BOX', box);
+    
+    document.body.removeChild(box)
+         
 }
 
-// container.onclick(
-//      setColor(color.colour)
-// )
+function addColor(){
+     console.log('Adding color has clicked');
 
+     const box = document.createElement('div')
+     box.id= 'box';
+     box.style.display= true;
+     box.style.width='40px'
+     box.style.height='40px'
+     
+     const intputBox = document.createElement('input')
+     intputBox.type= 'text';
+     box.appendChild(intputBox);
 
+     const button = document.createElement('button')
+     button.innerText= 'Submit'
+     box.appendChild(button)
 
+     box.style.backgroundColor= 'red'
+     
+     document.body.appendChild(box)
+     
+     button.addEventListener('click', ()=>{
+          console.log("Button Clicked");
+          const color = intputBox.value
+          //  intputBox.onchange(color= );
+         
+          if(color.length>  3){
+               console.log("SELECTED COLOR", color);
+               createColor(color)
+               
+          }
+          
+     })
+     
+}
 function displayingPannel(){
 // console.log(pannel);
+pannel.innerHTML = ''; // Clear previous buttons
 
      pannelColor.forEach((color)=>{
           
           const container = document.createElement('button');
-          container.style.width = '30px';
-          container.style.height = '20px';
+          container.style.width = '50px';
+          container.style.height = '30px';
+          
+          container.style.borderRadius='5px'
           container.style.backgroundColor = color.colour;
+          container.innerText = color.colour
+
+          container.addEventListener('click', ()=>setColor(color.colour))
           pannel.appendChild(container);
-          console.log(pannel);     
+             
      })
 }
 
-displayingPannel()
 
-console.log("Pannel", pannel);
+     displayingPannel();
+
+
+// Expose addColor to global scope so HTML can access it
+window.addColor = addColor;
+
